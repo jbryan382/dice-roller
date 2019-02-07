4249536403503a5005b5c0ae67d469c9a9b78755
Jordan Bryan <jbryan382@gmail.com>
2019-02-07 12:01:40 -0500

    Set up dice roller attempted rolling function

--- public/main.js
+++ public/main.js
@@ -1,7 +1,12 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+const rollDice = event => {
+  let valueDice = Math.floor(Math.random(0, 6))
+  const _h4 = document.createElement('h4')
+  _h4.textContent = valueDice
+  document.querySelector('h4').appendChild(_h4)
 }
 
+const main = () => {
+  const button = document.querySelector('.draw')
+  button.addEventListener('click', rollDice)
+}
 document.addEventListener('DOMContentLoaded', main)
